1b350b1b3cc1a1306b12633070d54ca9a46c0d34
Michael Sefranek <msefran@gmail.com>
2017-03-25 14:38:57 -0500

    fixed an error in create in sessions_controller hopefulyy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      login_url user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -14,4 +16,5 @@ class SessionsController < ApplicationController
 
   def destroy
   end
+
 end
