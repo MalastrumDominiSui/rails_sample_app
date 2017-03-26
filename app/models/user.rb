e0f05904ef30850d99f87e36c146ca04e5ed0c14
Michael Sefranek <msefran@gmail.com>
2017-03-25 21:37:23 -0500

    fixed remember function

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,14 +24,14 @@ class User < ActiveRecord::Base
   end
 
   # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
